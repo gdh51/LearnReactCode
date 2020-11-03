605d2ad2ff248c279ed24e36bbc45a961b4b28ae
ganhaobo <ganhaobo@meicai.cn>
2020-11-03 16:12:06 +0800

    update: hook

--- React/example/src/index.js
+++ React/example/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { FilterableProductTable } from './components/FilterableProductTable'
+import Hook from './example2-hook'
 import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<FilterableProductTable />, document.getElementById('root'))
+ReactDOM.render(<Hook />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
